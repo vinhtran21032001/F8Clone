92ef90b5299feb2edc132ef1441b14ffbf3b3d10
jiren <vinhtran21032001@gmail.com>
2021-09-07 20:57:02 +0700

    sencond commit2

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const loginRoute = require('./login')
 function route(app) {
     app.use('/courses', courseRoute);
     app.use('/login', loginRoute)
-    app.use('/', courseLitmitRoute)
+    app.use('/https://f8site-clone.herokuapp.com/', courseLitmitRoute)
 }
 
 module.exports = route;
